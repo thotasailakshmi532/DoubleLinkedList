e109c8195147d163eef6ba273f05d00a89c28d2b
thotasailakshmi532 <thotasailakshmi532@gmail.com>
2023-10-30 15:55:31 +0530

    Add project files.

---
+++ DoubleLinkedList/Program.cs
@@ -0,0 +1,53 @@
+﻿using DoubleLinkedListLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+            Console.ReadLine();
+        }
+
+        public static void runApp()
+        {
+            DoublyLinkedList list = new DoublyLinkedList();
+            Node n = new Node();
+            n.Data = "root";
+            list.add(n);
+            for (int i = 0; i < 10; i++)
+            {
+                Node node = new Node();
+                node.Data = Convert.ToString(i);
+                list.add(node);
+            }
+
+            Console.WriteLine("length of singly linked list=" + list.getLengthOfList());
+            Console.WriteLine("Traversing forward..");
+            n = list.getRoot();
+            while (n != null)
+            {
+                Console.WriteLine(n.Data);
+                n = list.getNext();
+            }
+
+
+            Console.WriteLine("Traversing backwards..");
+            n = list.getPrev();
+            while (n != null)
+            {
+                Console.WriteLine(n.Data);
+                n = list.getPrev();
+            }
+
+        }
+
+    }
+}
+
